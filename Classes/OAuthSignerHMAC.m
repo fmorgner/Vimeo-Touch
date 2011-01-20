84fe82f5a31a2807179feb76fe6bfb05af695bae
Felix Morgner <felix.morgner@gmail.com>
2011-01-20 10:02:47 +0100

    refactored OAuthHMAC to OAuthSignerHMAC to get better naming consitency

---
+++ Classes/OAuthSignerHMAC.m
@@ -0,0 +1,28 @@
+//
+//  OAuthHMAC.m
+//  Vimeo Touch
+//
+//  Created by Felix Morgner on 14.01.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import "OAuthSignerHMAC.h"
+
+
+@implementation OAuthSignerHMAC
+
++ (NSString*)signClearText:(NSString*)theClearText withSecret:(NSString*)theSecret
+	{
+	NSData* secretData = [theSecret dataUsingEncoding:NSUTF8StringEncoding];
+	NSData* clearTextData = [theClearText dataUsingEncoding:NSUTF8StringEncoding];
+	
+	unsigned char HMACResult[20];
+	
+	CCHmac(kCCHmacAlgSHA1, [secretData bytes], [secretData length], [clearTextData bytes], [clearTextData length], HMACResult);
+	
+	NSData* signedData = [NSData dataWithBytes:HMACResult length:20];
+	
+	return [signedData base64EncodedString];
+	}
+
+@end
