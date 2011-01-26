847a8cb0faaea4448b02f641f2a0fd12c827f64a
Felix Morgner <felix.morgner@gmail.com>
2011-01-26 17:33:58 +0100

    added single-include OAuth.g header

---
+++ Classes/OAuth.h
@@ -0,0 +1,16 @@
+//
+//  OAuth.h
+//  Vimeo Touch
+//
+//  Created by Felix Morgner on 26.01.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "OAuthConsumer.h"
+#import "OAuthParameter.h"
+#import "OAuthRequest.h"
+#import "OAuthSignerHMAC.h"
+#import "OAuthSignerProtocol.h"
+#import "OAuthToken.h"
