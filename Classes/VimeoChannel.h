6601f60e4dcf208731ab2fa0e52cbf0dc22b417b
Felix Morgner <felix.morgner@gmail.com>
2011-01-26 16:52:30 +0100

    added VimeoChannel class

---
+++ Classes/VimeoChannel.h
@@ -0,0 +1,17 @@
+//
+//  VimeoChannel.h
+//  Vimeo Touch
+//
+//  Created by Felix Morgner on 26.01.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface VimeoChannel : NSObject {
+@private
+    
+}
+
+@end
---
+++ Classes/VimeoChannel.m
@@ -0,0 +1,14 @@
+//
+//  VimeoChannel.m
+//  Vimeo Touch
+//
+//  Created by Felix Morgner on 26.01.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import "VimeoChannel.h"
+
+
+@implementation VimeoChannel
+
+@end
