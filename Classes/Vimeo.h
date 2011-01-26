752a48bacdcb3f7c3e9c8f12ac536e6d63ae12cf
Felix Morgner <felix.morgner@gmail.com>
2011-01-26 16:50:04 +0100

    added vimeo class which will provide access to all of the api methods supplied by vimeo

---
+++ Classes/Vimeo.h
@@ -0,0 +1,17 @@
+//
+//  Vimeo.h
+//  Vimeo Touch
+//
+//  Created by Felix Morgner on 26.01.11.
+//  Copyright 2011 Felix Morgner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Vimeo : NSObject {
+@private
+    
+}
+
+@end
