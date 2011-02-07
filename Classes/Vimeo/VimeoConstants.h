a9de2b0f892d762efa3675ed877cc8282f29fc8d
Felix Morgner <felix.morgner@gmail.com>
2011-02-07 20:25:50 +0100

    clean-up

--- Classes/Vimeo/VimeoConstants.h
+++ Classes/Vimeo/VimeoConstants.h
@@ -8,13 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+// Vimeo keychain access error domain
+static NSString* kVimeoKeychainAccessErrorDomain = @"VimeoKeychainAccessErrorDomain";
+
+// Vimeo application access permissions
 static NSString* kVimeoPermissionRead = @"read";
 static NSString* kVimeoPermissionWrite = @"write";
 static NSString* kVimeoPermissionDelete = @"delete";
 
+// Vimeo API Endpoints
 static NSString* kVimeoUserAuthorizationURL = @"http://vimeo.com/oauth/authorize?oauth_token=%@&permission=%@";
 static NSString* kVimeoAccessTokenVerificationURL = @"http://vimeo.com/oauth/access_token";
 static NSString* kVimeoAccessTokenRequestURL = @"http://vimeo.com/oauth/request_token";
+static NSString* kVimeoRestURL = @"http://vimeo.com/api/rest/v2";
 
 static NSString* kVimeoAuthorizationVerifierNotification = @"VimeoAuthorizationViewControllerDidReceiveVerifier";
 static NSString* kVimeoVerifierKey = @"vimeoVerifier";
+
+// Vimeo API methods
+static NSString* kVimeoMethodOAuthCheckAccessToken = @"vimeo.oauth.checkAccessToken";
