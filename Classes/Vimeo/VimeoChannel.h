10dca40996bf861d7ed010ac4dca417355ef27c0
Felix Morgner <felix.morgner@gmail.com>
2011-02-14 21:35:58 +0100

    added properties and some initialisation

--- Classes/Vimeo/VimeoChannel.h
+++ Classes/Vimeo/VimeoChannel.h
@@ -7,11 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TouchXML.h"
 
 
-@interface VimeoChannel : NSObject {
-@private
-    
-}
+@interface VimeoChannel : NSObject
+	{
+	NSUInteger ID;
+	BOOL featured;
+	BOOL sponsored;
+	
+	NSString* name;
+	NSString* desc;
+	NSDate* createdOn;
+	NSDate* modifiedOn;
+
+	NSUInteger videoCount;
+	NSUInteger subscriberCount;
+	
+	NSURL* logoURL;
+	NSURL* badgeURL;
+	NSURL* url;
+	}
+
+- (id)initWithXMLElement:(CXMLElement*)aElement;
+
+@property(nonatomic,assign) NSUInteger ID, videoCount, subscriberCount;
+@property(nonatomic,copy) NSString* name, *desc;
+@property(nonatomic,copy) NSURL* logoURL, *badgeURL, *url;
+@property(nonatomic,copy) NSDate* createdOn, *modifiedOn;
+@property(nonatomic,assign,getter = isFeatured) BOOL featured;
+@property(nonatomic,assign,getter = isSponsored) BOOL sponsored;
 
 @end
--- Classes/Vimeo/VimeoChannel.m
+++ Classes/Vimeo/VimeoChannel.m
@@ -11,4 +11,37 @@
 
 @implementation VimeoChannel
 
+@synthesize ID;
+@synthesize featured;
+@synthesize sponsored;
+
+@synthesize name;
+@synthesize desc;
+@synthesize createdOn;
+@synthesize modifiedOn;
+
+@synthesize videoCount;
+@synthesize subscriberCount;
+
+@synthesize logoURL;
+@synthesize badgeURL;
+@synthesize url;
+
+- (id)initWithXMLElement:(CXMLElement*)aElement
+	{
+	if((self = [super init]))
+		{
+		NSError* error = nil;
+		self.ID = [[[aElement nodeForXPath:@"attribute::id" error:nil] stringValue] intValue];
+		self.featured = [[[aElement nodeForXPath:@"attribute::is_featured" error:nil] stringValue] boolValue];
+		self.sponsored = [[[aElement nodeForXPath:@"attribute::is_sponsored" error:nil] stringValue] boolValue];
+		
+		[self setName:[[aElement nodeForXPath:@"name" error:nil] stringValue]];
+		[self setDesc:[[aElement nodeForXPath:@"description" error:nil] stringValue]];
+		[error release];
+		}
+	return self;
+	}
+
+
 @end
